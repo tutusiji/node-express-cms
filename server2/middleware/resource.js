0d0fc2dba42224da7c6ad65e8cf755973ee33fea
tutusiji <38522452+tutusiji@users.noreply.github.com>
2023-12-28 17:41:00 +0800

    no message

--- server2/middleware/resource.js
+++
@@ -1,7 +0,0 @@
-module.exports = options => {
-  return async (req, res, next) => {
-    const modelName = require('inflection').classify(req.params.resource)
-    req.Model = require(`../models/${modelName}`)
-    next()
-  }
-}
--- server2/models/Item.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = new mongoose.Schema({
-  name: { type: String },
-  icon: { type: String },
-})
-
-module.exports = mongoose.model('Item', schema)
