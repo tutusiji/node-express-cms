9017296fbdd6b86bc0d56f36fde9c3a070933022
tutusiji <38522452+tutusiji@users.noreply.github.com>
2024-01-17 23:21:50 +0800

    文件更新

--- admin/src/views/AdminUserList.vue
+++ admin/src/views/AdminUserList.vue
@@ -38,7 +38,7 @@ export default {
       this.items = res.data;
     },
     async handleDelete(row) {
-      this.$confirm(`是否确定删除“${row.name}”?`, "提示", {
+      this.$confirm(`是否确定删除“${row.username}”?`, "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning",
